64286e60832eb14b74e2be3f796c356781183eac
Chris Daly <chrisdaly1988@gmail.com>
2016-12-08 09:33:51 -0800

    Commit #8
    Finished 3 cols

--- new-app/ui.R
+++ new-app/ui.R
@@ -2,44 +2,22 @@ library(shinythemes)
 
 fluidPage(
   navbarPage(
-    theme = shinytheme("spacelab"), "RShiny",
-    
-    tabPanel("Home",
-             sidebarPanel(
-               fileInput("user_file", "File input:"),
-               tags$hr()
+    theme = shinytheme("spacelab"), "Sony - RShiny",
+  
+    fluidRow(
+      column(3,
+             h3("User data"),
+             fileInput("user_file", "File input:")
              ),
-             
-             mainPanel(
-               tabsetPanel(
-                 tabPanel("Data",
-                          h3("Input data preview"),
-                          tableOutput("user_data"),
-                          
-                          
-                          
-                          # Predictions.
-                          h3("Model predictions"),
-                          tableOutput('model_predictions'),
-                          
-                          downloadButton('downloadData', 'Download')
-                 ),
-                 tabPanel("Test",
-                          h4("Table"),
-                          tableOutput("table"),
-                          h4("Verbatim text output"),
-                          verbatimTextOutput("txtout"),
-                          h1("Header 1"),
-                          h2("Header 2"),
-                          h3("Header 3"),
-                          h4("Header 4"),
-                          h5("Header 5"),
-                          tags$code('SALES_PHY')),
-                 tabPanel("Tab 3")
-               )
-             )
-    ),
-    tabPanel("Codebook"),
-    tabPanel("About")
-  )
-)
+      column(5,
+             h3("User data preview"),
+             tableOutput("user_data")
+            ),
+      column(4,
+             # Predictions.
+             h3("Model predictions"),
+             tableOutput('model_predictions'),
+             downloadButton('downloadData', 'Download')
+            )
+    )
+))
